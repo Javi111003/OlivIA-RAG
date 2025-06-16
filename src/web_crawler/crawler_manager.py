751bd51255ce0feaba000e10ed275531e0e6379c
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-06-16 16:32:15 -0400

    feat: Added kg section and data_preparation section with  text cleaner y document loader

--- src/web_crawler/crawler_manager.py
+++ src/web_crawler/crawler_manager.py
@@ -1,1 +1,5 @@
 # ...existing code...
+# crawler_manager.py: Investigar y decidir una librería de crawling 
+    #   (ej., Scrapy, o implementaciones más ligeras con requests + BeautifulSoup). 
+#   Su tarea principal será construir un sistema que pueda navegar la web,
+#   identificar URLs de interés y descargarlas
