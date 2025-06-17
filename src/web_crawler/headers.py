dc6296402b29ac8f30d257c52e4ff27c47b3ae97
josemiley02 <leyvadelacruzjosemiguel20@gmail.com>
2025-06-17 19:25:35 -0400

    feat/Crawler and Scraper init version

---
+++ src/web_crawler/headers.py
@@ -0,0 +1,16 @@
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/122.0.0.0 Safari/537.36"
+    ),
+    "Accept": (
+        "text/html,application/xhtml+xml,application/xml;"
+        "q=0.9,image/webp,image/apng,*/*;q=0.8"
+    ),
+    "Accept-Language": "es-ES,es;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "DNT": "1",  # Do Not Track
+    "Upgrade-Insecure-Requests": "1"
+}
