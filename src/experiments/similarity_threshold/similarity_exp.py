5d4cc85b066d899629a490e8860aa9286bd29175
Javi111003 <128992433+Javi111003@users.noreply.github.com>
2025-07-08 04:04:02 -0400

    Generated threshold similarity test results

--- src/experiments/similarity_threshold/similarity_exp.py
+++ src/experiments/similarity_threshold/similarity_exp.py
@@ -6,7 +6,7 @@ from pathlib import Path
 src_path = Path(__file__).parent.parent.parent  # Ajusta según tu estructura
 sys.path.append(str(src_path))
 
-
+import asyncio
 import json
 from typing import Any, Dict, List
 from pydantic import BaseModel
@@ -14,13 +14,13 @@ from generator.llm_provider import MistralLLMProvider
 from retriever.dense_retriever import DenseRetriever
 from retriever.query_processor import QueryProcessor
 from embedding_models.embedding_generator import EmbeddingGenerator
-from queries_thexp import *
+from .queries_thexp import *
 
 
 class ChunkLabel(BaseModel):
     is_relevant: bool
 
-def _use_llm_for_evaluation(query: str, document: str, llm: MistralLLMProvider) -> bool:
+async def _use_llm_for_evaluation(query: str, document: str, llm: MistralLLMProvider) -> bool:
     llm = llm.with_structured_output(ChunkLabel)
     prompt = f"""
     Pregunta: "{query}"
@@ -31,10 +31,10 @@ def _use_llm_for_evaluation(query: str, document: str, llm: MistralLLMProvider) 
 
     Responde solo con un JSON que contenga el campo booleano `is_relevant`.
     """
-    result: ChunkLabel = llm.invoke(prompt)
+    result: ChunkLabel = await llm.ainvoke(prompt)
     return result.is_relevant
 
-def evaluate_query(query_embedding,
+async def evaluate_query(query_embedding,
                    query: str, 
                    retriever: DenseRetriever, 
                    llm: MistralLLMProvider) -> Dict[str, Any]:
@@ -42,7 +42,7 @@ def evaluate_query(query_embedding,
     doc_scores = retriever.retrieve(query_embedding, top_k= 10)
     for doc, score in doc_scores:
         chunk_text = doc
-        is_relevant = _use_llm_for_evaluation(query_embedding, chunk_text, llm)
+        is_relevant = await _use_llm_for_evaluation(query_embedding, chunk_text, llm)
         chunk_score.append({
             "text": chunk_text,
             "score": score,
@@ -53,14 +53,14 @@ def evaluate_query(query_embedding,
         "scores": chunk_score
     }
 
-def run_experiments(queries: list[str]) -> list[Dict[str, Any]]:
+async def run_experiments(queries: list[str]) -> list[Dict[str, Any]]:
     retriever = DenseRetriever()
     llm = MistralLLMProvider()
     gen = EmbeddingGenerator()
     results = []
     for query in queries:
         query_embedding = gen.generate_embedding(query)
-        result = evaluate_query(query_embedding, query, retriever, llm)
+        result = await evaluate_query(query_embedding, query, retriever, llm)
         results.append(result)
     return results
 
@@ -72,9 +72,15 @@ def save_results(
         json.dump(results, f, indent=2, ensure_ascii=False)
     print(f"[✅] Resultados guardados en {filepath}")
 
-def run_threshold_experiment(
+async def run_threshold_experiment(
     queries: List[str] = queries_ste,
     output_path: str = "./src/experiments/similarity_threshold/results.json",
     ):
-    results = run_experiments(queries)
+    results = await run_experiments(queries)
     save_results(results, output_path)
+    
+if __name__ == "__main__":
+    asyncio.run(run_threshold_experiment(
+        queries=queries_ste,
+        output_path="./src/experiments/similarity_threshold/results.json"
+    ))
